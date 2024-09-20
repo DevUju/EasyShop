60ad6d9f7cf423dadc7b72d257425b6d4225f0b0
Obianuju1998 <agnesanosike@gmail.com>
2024-09-20 15:19:34 +0100

    feat: implemented product app

--- EasyShop/settings.py
+++ EasyShop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user_authentication',
+    'products',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
 ]
---
+++ products/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Category, Product
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+    list_filter = ['category']
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
